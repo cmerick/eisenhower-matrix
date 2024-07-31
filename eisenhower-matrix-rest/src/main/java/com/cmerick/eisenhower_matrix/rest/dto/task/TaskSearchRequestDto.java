89821ceb0324628cd7ddb08e9de1f6ed86c7c208
Erick Castilho <erickcastilhomestre@gmail.com>
2024-07-31 16:24:45 -0300

    merge

---
+++ eisenhower-matrix-rest/src/main/java/com/cmerick/eisenhower_matrix/rest/dto/task/TaskSearchRequestDto.java
@@ -0,0 +1,9 @@
+package com.cmerick.eisenhower_matrix.rest.dto.task;
+
+import lombok.Builder;
+import lombok.Data;
+
+@Data
+@Builder
+public class TaskSearchRequestDto {
+}
--- eisenhower-matrix-rest/src/main/java/com/cmerick/eisenhower_matrix/rest/service/TaskService.java
+++ eisenhower-matrix-rest/src/main/java/com/cmerick/eisenhower_matrix/rest/service/TaskService.java
@@ -1,9 +1,14 @@
 package com.cmerick.eisenhower_matrix.rest.service;
 
+import com.cmerick.eisenhower_matrix.rest.dto.task.TaskResponse;
+import com.cmerick.eisenhower_matrix.rest.dto.task.TaskSearchRequestDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
 public class TaskService {
+    public TaskResponse findAll(TaskSearchRequestDto filter) {
+        return TaskResponse.builder().build();
+    }
 }
